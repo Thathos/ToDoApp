32ab348de76fc5bc3f4026640928afe8c5331361
REGINALD COLE <rc0551329@otc.edu>
2026-04-27 00:30:50 -0500

    messing with dom stuff

--- server.js
+++ server.js
@@ -5,3 +5,27 @@ const app = express(); //creating an express app
 app.use(express.json()); //parse json bodies
 
 app.use(express.static('pubic')); //serve static files from "public" folder
+
+const port = 3000; //defining our port
+
+app.get('/', (req, res) =>{
+    res.send('Hello from Express!')
+});
+
+//GET request returns an array of items
+app.get("/api/tasks", (req, res) =>{
+    const items = [
+        {id: 1, name: "Take out trash"},
+        {id: 2, name: "Pay rent bill"},
+    ];
+    res.json(items);
+})
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on http://localhost:${port}`)
+});
